d7eea1ce8b593c2e63fa61686c41af7026ede4bb
Efimov <ybik2013@mail.ru>
2023-01-18 15:08:04 +0800

    Complete №27

---
+++ HomeWork4/27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №27 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+void GetSummNumber(int N)
+{
+    Console.Write($"Сумма цифр в числе {N} = ");
+    int sum = 0;
+    while(N > 0)
+    {
+        sum += N % 10;
+        N /= 10;
+    }
+    Console.Write(sum);
+}
+
+Console.Clear();
+Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+GetSummNumber(num);
