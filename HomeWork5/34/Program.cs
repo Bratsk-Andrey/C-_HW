1ead0912b5a03189a159e29a1dd76cac80b3bd97
Efimov <ybik2013@mail.ru>
2023-01-18 16:38:31 +0800

    Complete №34

---
+++ HomeWork5/34/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача №34
+// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве. 
+class HM5_34
+{
+    static void Main()
+    {
+        Console.Clear();
+        Console.WriteLine("Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.");
+        int size = 6, count = 0;
+        int[] array = new int[size];
+        Random rnd = new Random();
+        Console.Write("Массив заполеннный случайными положительными трехзначными числами: ");
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[i] = rnd.Next(100, 1000);
+            Console.Write(array[i] + " ");
+        }
+        for(int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            count++;
+        }
+        Console.WriteLine();
+        Console.Write($"Количество чётных чисел в массиве: {count}");
+    }
+}
