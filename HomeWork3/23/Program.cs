1e3e2879277213c48dc9822e23f9876381feedbc
Efimov <ybik2013@mail.ru>
2023-01-18 14:51:44 +0800

    Complete №25

--- HomeWork3/23/Program.cs
+++ HomeWork3/23/Program.cs
@@ -7,14 +7,14 @@
         Console.WriteLine("Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.");
         Console.Write("Введите число: ");
         int num1 = int.Parse(Console.ReadLine());
-        int cub1 = 0;
+        //int cub1 = 0;
         int index1 = 1;
 
         while(index1 <= num1)
         {
-            cub1 = index1 * index1 * index1;
-            Console.Write(" " + cub1);
-            index1++;
+            //cub1 = index1 * index1 * index1;
+            Console.Write(" " + Math.Pow(index1++, 3));
+            //index1++;
         }
     }
 }
