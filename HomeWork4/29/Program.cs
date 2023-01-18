bea7aa6734c2d2a430e95878ed5b375725fb4413
Efimov <ybik2013@mail.ru>
2023-01-18 15:14:40 +0800

    Complete №29

---
+++ HomeWork4/29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача №29
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+Console.WriteLine("Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.");
+int size = 8;
+int[] array = new int[size];
+Random rnd = new Random();
+Console.Write("Массив из 8 элементов: ");
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(-100, 100);
+    Console.Write(array[i] + " ");
+}
