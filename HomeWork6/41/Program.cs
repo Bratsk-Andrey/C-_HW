35b3201fe66816406efd0d1853df711f2a2f267b
Efimov <ybik2013@mail.ru>
2023-02-01 15:33:15 +0800

    Complete HomeWork6

---
+++ HomeWork6/41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача №41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.Write("Введите количество цифр: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[M];
+int count = 0;
+
+for(int i = 0; i < array.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент массива: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Полученный массив: ");
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+Console.WriteLine();
+for(int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    count++;
+}
+Console.WriteLine($"Количество элементов больше 0: {count}");
---
+++ HomeWork6/43/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №43
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2, k2 задаются пользователем.
+
+Console.Clear();
+double k1, k2, b1, b2;
+Console.Write("Введите значение k1: ");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения ({x}; {y})");
